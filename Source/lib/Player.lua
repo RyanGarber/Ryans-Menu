ca037c2edfc8c25eb75309863bc7c0fb531bc0c8
Ryan Garber <ryanmichaelgarber@gmail.com>
2022-08-16 18:52:38 -0400

    Further improve Super Crash

--- Source/lib/Player.lua
+++ Source/lib/Player.lua
@@ -150,17 +150,12 @@ function Player.super_crash(self, block_syncs)
     local starting_coords = user.coords
 
     local crash = function()
-        local coords = v3(self.coords)
         ENTITY.SET_ENTITY_VISIBLE(user.ped_id, false)
-        ENTITY.SET_ENTITY_COORDS_NO_OFFSET(user.ped_id, coords.x, coords.y, coords.z + 50, false, false, false)
+        local coords = v3.new()
+        PATHFIND.GET_CLOSEST_VEHICLE_NODE(self.coords.x, self.coords.y, self.coords.z, coords, 1, 3.0, 0)
+        Ryan.Teleport(coords, false)
 
         util.yield(500)
-        local raycast = Ryan.Raycast(v3(coords.x, coords.y, 1250), v3(0, 0, -1), 2500, Ryan.RaycastFlags.All)
-        if raycast.did_hit then coords:set(raycast.hit_coords)
-        else Ryan.Toast("Could not find the ground.\nTry using Super Crash closer to the target.") end
-        ENTITY.SET_ENTITY_COORDS_NO_OFFSET(user.ped_id, coords.x, coords.y, coords.z, false, false, false)
-
-        util.yield(250)
         PLAYER.SET_PLAYER_PARACHUTE_PACK_MODEL_OVERRIDE(user.id, bush)
         PED.SET_PED_COMPONENT_VARIATION(user.ped_id, 5, 8, 0, 0)
         
@@ -168,7 +163,7 @@ function Player.super_crash(self, block_syncs)
         PLAYER.CLEAR_PLAYER_PARACHUTE_PACK_MODEL_OVERRIDE(user.id)
         
         util.yield(2000)
-        ENTITY.SET_ENTITY_COORDS_NO_OFFSET(user.ped_id, coords.x, coords.y, coords.z, false, false, false)
+        Ryan.Teleport(coords, false)
         PLAYER.SET_PLAYER_PARACHUTE_PACK_MODEL_OVERRIDE(user.id, bush)
         PED.SET_PED_COMPONENT_VARIATION(user.ped_id, 5, 31, 0, 0)
         
